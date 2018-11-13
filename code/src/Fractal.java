393a87263298a4a98f2222efdca5bb7aab6451bd
Dilshani <39186410+DDilshani@users.noreply.github.com>
2018-11-13 09:18:39 +0530

    Update Fractal.java

--- code/src/Fractal.java
+++ code/src/Fractal.java
@@ -1,12 +1,17 @@
 class Fractal {     
+
     public static void main(String [] args) {
-        
+
+
+
     	if(args.length>0 && args[0]=="Mandelbrot"){
        		if( args.length==1){ // o arguments >> calling default values
        			//-------default values
        			//Mandelbrot();
 
        		}else if( args.length==4){ //4 arguments >> region of interst in the complex plane
+      			
+
       			//-------complex plane
       			//(float realMin,float realMax,float imgMin,float imgMax)
       		
@@ -15,7 +20,7 @@ class Fractal {
       			//(float realMin,float realMax,float imgMin,float imgMax,int maxIter)
 
       		}else {
-      			System.out.pritnln("wrong input-Mandelbrot");
+      			System.out.println("wrong input-Mandelbrot");
       		}
 
       	}else if (args.length>0 && args[0]=="Julia"){
@@ -28,35 +33,36 @@ class Fractal {
       			//(float cR,float cI)
       		
       		}else {
-      			System.out.pritnln("wrong input-Julia");
+      			System.out.println("wrong input-Julia");
       		}
       	}else {
-      			System.out.pritnln("wrong input");
+      			System.out.println("wrong input");
       	}
 
     }
 }
+    
+    class Debugger{
 
-
-public void debugArgs(Mandelbrot m){
-    		System.out.pritnln("Mandelbrot");
-    		System.out.pritnln("Screen width" + m.screenWidth);
-    		System.out.pritnln("Real min" + m.realMin);
-    		System.out.pritnln("Real max" + m.realMax);
-    		System.out.pritnln("Complex min" + m.compMin);
-    		System.out.pritnln("Complex max" + m.compMax);
-    		System.out.pritnln("Iterations" + m.itter);
+    	public void debugArgs(Mandelbrot m){
+    		System.out.println("Mandelbrot");
+    		System.out.println("Screen width" + m.screenWidth);
+    		System.out.println("Real min" + m.realMin);
+    		System.out.println("Real max" + m.realMax);
+    		System.out.println("Complex min" + m.compMin);
+    		System.out.println("Complex max" + m.compMax);
+    		System.out.println("Iterations" + m.itter);
 
     	}
 
     	public void debugArgs(Julia j){
-    		System.out.pritnln("Julia");
-    		System.out.pritnln("Screen width" + j.screenWidth);
-    		System.out.pritnln("Real min" + j.realMin);
-    		System.out.pritnln("Real max" + j.realMax);
-    		System.out.pritnln("Complex min" + j.compMin);
-    		System.out.pritnln("Complex max" + j.compMax);
-    		System.out.pritnln("Iterations" + j.itter);
+    		System.out.println("Julia");
+    		System.out.println("Screen width" + j.screenWidth);
+    		System.out.println("Real min" + j.realMin);
+    		System.out.println("Real max" + j.realMax);
+    		System.out.println("Complex min" + j.compMin);
+    		System.out.println("Complex max" + j.compMax);
+    		System.out.println("Iterations" + j.itter);
 
     	}
-}
+    }
