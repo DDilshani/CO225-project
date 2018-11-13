66038e9b37c325ded2a31f90d8802e33d4910cc8
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-11-13 22:34:48 +0530

    working

--- code/src/Fractal.java
+++ code/src/Fractal.java
@@ -4,31 +4,8 @@ class Fractal {
 
         Mandelbrot m = new Mandelbrot();
         Debugger.debugArgs(m);
-
         m.draw();
     }
 }
 
-class Debugger {
 
-    public static void debugArgs(Mandelbrot m) {
-        System.out.println("Mandelbrot");
-        System.out.println("Screen width " + m.screenWidth);
-        System.out.println("Real min " + m.realMin);
-        System.out.println("Real max " + m.realMax);
-        System.out.println("Complex min " + m.compMin);
-        System.out.println("Complex max " + m.compMax);
-        System.out.println("Iterations " + m.itter);
-
-    }
-
-    /*public  static  void debugArgs(Julia j) {
-        System.out.println("Julia");
-        System.out.println("Screen width " + j.screenWidth);
-        System.out.println("Real min " + j.realMin);
-        System.out.println("Real max " + j.realMax);
-        System.out.println("Complex min " + j.compMin);
-        System.out.println("Complex max " + j.compMax);
-        System.out.println("Iterations " + j.itter);
-    }*/
-}
--- code/src/Mandelbrot.java
+++ code/src/Mandelbrot.java
@@ -1,7 +1,9 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.util.Random;
 
-
-public class Mandelbrot {
+public class Mandelbrot extends JPanel {
 
     public float realMin = -1;
     public float realMax = 1;
@@ -10,24 +12,15 @@ public class Mandelbrot {
     public float compMax = 1;
 
     public int itter = 10;
-    public int screenHeight = 600;
-    public int screenWidth = 600;
+    public int screenHeight = 300;
+    public int screenWidth = 400;
 
-    private String title = "Mandelbrot";
+    String title = "Mandelbrot";
 
     static JFrame frame;
 
     Mandelbrot() {
-
-        // Load default values
-        this.realMin = -1;
-        this.realMax = 1;
-
-        this.compMin = -1;
-        this.compMax = 1;
-
-        this.itter = 1;
-
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
     }
 
     Mandelbrot(float realMin, float realMax, float compMin, float compMax, int itter) {
@@ -40,6 +33,7 @@ public class Mandelbrot {
         this.compMax = compMax;
 
         this.itter = itter;
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
     }
 
     public void draw() {
@@ -47,11 +41,56 @@ public class Mandelbrot {
         // Draw the window
         frame = new JFrame(this.title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(new Panel(this.screenWidth, this.screenHeight,this));
+        frame.setContentPane(this);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
 
+
+    public static void printPoint(Graphics2D frame, Color c, Point p) {
+
+        frame.setColor(c);
+        frame.draw(new Line2D.Double(p.getX(), p.getY(), p.getX(), p.getY()));
+    }
+
+    public void paintComponent(Graphics g) {
+        // call paintComponent from parent class
+        super.paintComponent(g);
+
+        for (int x = 0; x < screenWidth; x++) {
+            for (int y = 0; y < screenHeight; y++) {
+                Point p = new Point(x, y);
+
+                float zX = 2 * (x - screenWidth / 2f) / (float) screenWidth;
+                float zY = 2 * (y - screenHeight / 2f) / (float) screenHeight;
+                boolean flag = false;
+
+                Complex c = new Complex(zX, zY);
+                Complex z = new Complex(0, 0);
+
+                // now run 1000 iterations
+                for (int k = 0; k < 1000; k++) {
+
+                    //z = z^2 + c
+                    z = Complex.add(Complex.square(z), c);
+
+                    if (z.abs() > 2) {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                System.out.println("x: (" + x + ") " + zX + " y: (" + y + ") " + zY + " --> " + flag);
+
+                if (flag) {
+                    printPoint((Graphics2D) g, Color.BLACK, p);
+                } else {
+                    printPoint((Graphics2D) g, Color.WHITE, p);
+                }
+
+            }
+        }
     }
 
 
