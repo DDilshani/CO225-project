3f0f2ce629bad13130ab3d16135ae08b988c18e3
DilshaniKarunarathna <dinelkadilshani95@gmail.com>
2018-12-19 11:21:02 +0530

    list update

--- project_2/code/src/StockDatabase.java
+++ project_2/code/src/StockDatabase.java
@@ -4,39 +4,94 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class StockDatabase {
+class Company{
+    String symbol, name, bidBy;
+    double price;
 
-    // make this static ???
-    // private ArrayList<Contact> contactList = new ArrayList<>();
+    public Company(String symbol, String name, double price,String bidBy){
+    this.symbol=symbol;
+    this.name=name;
+    this.price=price;
+    this.bidBy=bidBy;
+    }
+
+    public String toString() {
+        return  symbol + " "+name+ " "+price+ " "+bidBy;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSymbol(){
+        return symbol;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public String getBidBy(){
+        return name;
+    }
+
+    public void setPrice(double p){
+        price=p;
+    }
+
+    public boolean newBid(String bidder, double p){
+        if (p<=price){
+            return false;
+        }else{
+            bidBy=bidder;
+            price=p;
+            return true;
+        }
+    }
+}
+
+
+class History{
+    String symbol, bidBy;
+    double price;
+    Date time;
+
+    public History(String symbol, double price,String bidBy,Date time){
+    this.symbol=symbol;
+    this.price=price;
+    this.bidBy=bidBy;
+    this.time=time;
+    }
+
+    public String toString() {
+        return  symbol + " "+ " "+price+ " "+bidBy+" "+time;
+    }
+}
+
+public class StockDatabase {
 
     public StockDatabase(String filePath) {
         readCSV(filePath);
     }
 
     private void readCSV(String filePath) {
-        BufferedReader reader = null;
+        Scanner sc = new Scanner(System.in); 
+        String csvfile = "stocks.csv"; 
+        String line = "";
 
-        try {
-            reader = new BufferedReader(new FileReader(filePath));
-            reader.readLine();  // read the header line
+        LinkedList<History> myHistory = new LinkedList<History>();
+        Map<String, Company> stockMarket = new Hashtable<String, Company>(); //create and map hash table
 
-            String[] values;
-            String symbol, securityName, price;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvfile))) {
+            br.readLine(); //read first line
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                stockMarket.put(data[0],new Company(data[0],data[1],Double.parseDouble(data[2]),new String())); // enter data to the hash table
+                myHistory.add(new History(data[0],Double.parseDouble(data[2]),new String(),new Date()));
 
-            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                values = line.split(",");
-
-                symbol = values[0];
-                securityName = values[1];
-                price = values[2];
-
-                //System.out.println(symbol + "\t\t" + price + "\t\t" + securityName);
-
-                // Add them to a suotable collection
             }
-            reader.close();
 
-        } catch (IOException e) {
+        }catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
 
@@ -47,7 +102,11 @@ public class StockDatabase {
     }
 
     public void printAllCompanies() {
-        // Need to  code
+        Iterator<Integer> iterator = stockMarket.keySet().iterator();//iterate the hash table
+        while(iterator.hasNext()){
+            Company com = stockMarket.get(iterator.next());
+        }
+
     }
 
 
@@ -57,4 +116,5 @@ public class StockDatabase {
     }
 
     //public getCompany(){ }
+
 }
