c2caf68205ee3f89b5c1725110a88498b5e19acb
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-12-24 10:16:18 +0530

    Display was updated

--- project_2/src/Display.java
+++ project_2/src/Display.java
@@ -3,6 +3,7 @@ import javax.swing.Timer;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Display
@@ -14,15 +15,18 @@ public class Display
     private static JFrame frame;
 
     private final int screenWidth = 600;
-    private final int screenHeight = 600;
+    private final int screenHeight = 400;
     private final int noOfLables = 8;
 
-
-    JTextArea text;
     //VisualServer vServer;
     /*public enum compName {
         FB, VRTU, MSFT, GOOGL, YHOO, XLNX, TSLA, TXN
     }*/
+
+    private JLabel lblStockSelect;
+    private JTextArea lblSearchRes;
+    private JTextField txtStockSelect;
+    private JButton enterButton;
 
     private JLabel[] lblNames = new JLabel[noOfLables];
     private JLabel[] lblValues = new JLabel[noOfLables];
@@ -40,9 +44,7 @@ public class Display
 
         this.server = server;
 
-   }
-
-
+    }
 
     public void draw() {
 
@@ -50,37 +52,62 @@ public class Display
         frame = new JFrame("Auction Server");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(this);
-        //frame.pack();
 
+        // Title
         JLabel title = new JLabel("Auction Server");
         title.setBounds(50, 30, 300, 20);
         title.setFont(new Font("Verdana", Font.PLAIN, 24));
         frame.add(title);
 
-        JTextField txtstockSelect = new JTextField("");
-        txtstockSelect.setBounds(50, 325, 75, 20);
-        txtstockSelect.setFont(new Font("Verdana", Font.PLAIN, 24));
-        frame.add(txtstockSelect);
+        // Search Label
+        lblStockSelect = new JLabel("Security:");
+        lblStockSelect.setBounds(250, 75, 70, 25);
+        //lblStockSelect.setFont(new Font("Verdana", Font.PLAIN, 24));
+        frame.add(lblStockSelect);
 
-        
-        JButton enterButton = new JButton("Enter");
-        enterButton.setBounds(150, 325, 100, 20);
-        enterButton.setFont(new Font("Verdana", Font.PLAIN, 10));
+        // Search Box
+        txtStockSelect = new JTextField("");
+        txtStockSelect.setBounds(320, 75, 110, 25);
+        frame.add(txtStockSelect);
+
+        // Enter Button
+        enterButton = new JButton("Enter");
+        enterButton.setBounds(435, 75, 70, 25);
         frame.add(enterButton);
 
-        /*------------------------
-        
-        declaring display area- textarea???
+        // Search Result
+        lblSearchRes = new JTextArea("");
+        lblSearchRes.setBounds(250, 110, 260, 200);
+        //lblSearchRes.setAutoscrolls(true);
+        frame.add(lblSearchRes);
 
 
-        -----------------------------*/
-       enterButton.addActionListener(new ActionListener() {
+        // Event Listener : Enter button, onClick
+        enterButton.addActionListener(new ActionListener() {
+            //TODO: This should be optimize
+
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                //call the method printSymbolHistory(String s);
+                String symbol = txtStockSelect.getText();
+                //StringBuilder sb = new StringBuilder("Results:\n\n");
+
+                if (stock.isSymbolExists(symbol)) {
+                    ArrayList<String> ls = stock.getSymbolHistory(symbol);
+
+                    for (int i = 0; i < ls.size(); i++) {
+                        //System.out.println(">> " + ls.get(i));
+                        //sb.append(ls.get(i));
+                        //sb.append("\n");
+                        lblSearchRes.append(ls.get(i) + "\n");
+                    }
+                    //lblSearchRes.setText(sb.toString());
+
+                } else {
+                    //System.out.println("The symbol, " + symbol + " is not exists.");
+                    lblSearchRes.setText("The symbol, " + symbol + " is not exists.");
+                }
             }
         });
-
 
 
         for (int i = 0; i < noOfLables; i++) {
